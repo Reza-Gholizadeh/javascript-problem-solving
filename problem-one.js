38e70551d5cdbcef9d4546970bb38fcf911b4e70
Reza Gholizadeh <reza.gholizadeh6@gmail.com>
2022-08-23 16:20:48 +0430

    solving first challenge

---
+++ problem-one.js
@@ -0,0 +1,7 @@
+// First Challeng
+
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+
+const myFunc = (str) => str.slice(-3)
